7bc403695e9d320cfb33a7f3aa677e88d0bfe0ce
KYH <drappear@gmail.com>
2024-08-29 12:17:28 +0900

    인터페이스

---
+++ object/src/inter/Fightable.java
@@ -0,0 +1,6 @@
+package inter;
+
+// 다중 상속
+public interface Fightable extends Movable, Attackable {
+
+}
---
+++ object/src/inter/RemoteControl.java
@@ -0,0 +1,24 @@
+package inter;
+
+public interface RemoteControl {
+    int MAX_VOLUME = 10;
+    int MIN_VOLUME = 0;
+
+    void turnOn();
+
+    void turnOff();
+
+    void setVolume(int volume);
+
+    default void setMute(boolean mute) {
+        if (mute) {
+            System.out.println("무음");
+        } else {
+            System.out.println("무음 해제");
+        }
+    }
+
+    static void changeBattery() {
+        System.out.println("건전지 교체");
+    }
+}
